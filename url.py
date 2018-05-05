b50e4d59b0ea6da4ce7c52c4c0d212cc5b6cb7fe
cuixinqiang <1752491054@qq.com>
2018-05-05 17:09:43 +0800

    “第二次提交”

---
+++ url.py
@@ -0,0 +1,9 @@
+from urllib import request
+
+with request.urlopen('https://weishangtech.com/#/') as f:
+    data = f.read()
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('----------------')
+    print('Data:', data.decode('utf-8'))
